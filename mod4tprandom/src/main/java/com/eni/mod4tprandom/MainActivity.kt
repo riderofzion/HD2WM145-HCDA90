7b9f4e2cff658fb6b90fb0e808a50b9434334826
quentinrouet@hotmail.fr <quentinrouet@hotmail.fr>
2022-11-03 11:35:51 +0100

    Module 4 Demo + TP 1

---
+++ mod4tprandom/src/main/java/com/eni/mod4tprandom/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.eni.mod4tprandom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var textViewResultat : TextView;
+    lateinit var editTextMin : EditText;
+    lateinit var editTextMax : EditText;
+    lateinit var buttonGenerate : Button;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textViewResultat = findViewById(R.id.textViewResultat);
+        editTextMin = findViewById(R.id.editTextMin);
+        editTextMax = findViewById(R.id.editTextMax);
+        buttonGenerate = findViewById(R.id.buttonGenerate);
+        //Lors du clic sur le bouton Generate, je génère un nombre aléatoire entre min et max
+        buttonGenerate.setOnClickListener {
+            try {
+                val min = Integer.parseInt(editTextMin.text.toString())
+                val max = Integer.parseInt(editTextMax.text.toString())
+                if(min > max)
+                    textViewResultat.text = "Min doit être inférieur à max"
+                else
+                    textViewResultat.text = "Resultat : ${(min..max).random()}"
+            }catch(nfe : java.lang.NumberFormatException){
+                textViewResultat.text = "Les nombres rentrés ne sont pas des entiers !"
+            }
+
+        }
+
+    }
+}
---
+++ module4demo/src/main/java/com/eni/module4demo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.eni.module4demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.ToggleButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //ON va écrire du code ici :D
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            textView.text = "J'ai appuyé sur le bouton :D"
+        }
+
+        //val toggleButton = findViewById<ToggleButton>(R.id.button)
+        //toggleButton.setOnCheckedChangeListener { _, isChecked ->
+        //    if(isChecked) {
+        //        textView.text = "J'ai appuyé sur le bouton :D"
+        //    }else
+        //        textView.text = "Hello World"
+        //}
+    }
+}
